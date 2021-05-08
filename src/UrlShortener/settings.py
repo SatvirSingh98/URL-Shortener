5f59d183c5a985d6d9ded21d59fddc39f49b5119
satvir <satvirsingh.juc@gmail.com>
2021-05-08 12:01:19 +0530

    changed allowed hosts

--- src/UrlShortener/settings.py
+++ src/UrlShortener/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-i25(my=z#v5bl2=)ij5_+qf_kd#^_+$$kr3n@g=d-mmt$-w*zo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
