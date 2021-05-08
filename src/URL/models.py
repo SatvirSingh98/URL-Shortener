094985929e0cd0e1cdced2a21f89e25111fe50a2
satvir <satvirsingh.juc@gmail.com>
2021-05-08 11:19:46 +0530

    Preventing slug duplication

--- src/URL/models.py
+++ src/URL/models.py
@@ -4,16 +4,29 @@ from django.db.models.signals import pre_save
 
 
 class UrlShortener(models.Model):
-    original_url = models.URLField(unique=True)
+    original_url = models.URLField(unique=True, db_index=True)
     slug = models.SlugField(unique=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.original_url
 
 
+def unique_slug_generator(instance, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = md5(instance.original_url.encode()).hexdigest()[:8]
+    qs = instance.__class__.objects.filter(slug=slug)
+    if qs.exists():
+        new_slug = md5(instance.original_url.encode()).hexdigest()[:8]
+        return unique_slug_generator(instance, new_slug)
+    return slug
+
+
 def pre_save_reciever(sender, instance, *args, **kwargs):
     if not instance.slug:
-        instance.slug = md5(instance.original_url.encode()).hexdigest()[:10]
+        instance.slug = unique_slug_generator(instance)
 
 
 pre_save.connect(pre_save_reciever, sender=UrlShortener)
